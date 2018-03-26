c13e46e101c19d17ee3159abdabdbcb884a07d57
ModyQyw <261857860@qq.com>
2018-03-26 20:37:37 +0800

    v2.1 更新数据库结构，更新插入数据，更新反馈查看页。

--- db_script/create.sql
+++ db_script/create.sql
@@ -58,5 +58,6 @@ create table excp (
 create table fb (
 	dat		datetime,					#时间
     id		int not null,				#学号
+    nam     varchar(200) not null,      #姓名
     kno		int not null				#知识点掌握程度，由完全掌握到完全不掌握1-5
 ) engine=InnoDB;
--- db_script/insert-fb.sql
+++ db_script/insert-fb.sql
@@ -2,10 +2,11 @@
 create table fb (
 	dat		datetime,					#时间
     id		int not null,				#学号
+    nam     varchar(200) not null,      #姓名
     kno		int not null				#知识点掌握程度，由完全掌握到完全不掌握1-5
-);
+) engine=InnoDB;
 */
 use `lab_sys`;
 select * from fb;
-insert into fb(dat,id,kno) values(now(),'16301027','2');
-insert into fb(dat,id,kno) values(now(),'16301023','4');
+insert into fb(dat,id,nam,kno) values(now(),'16301027','方宇蔚','2');
+insert into fb(dat,id,nam,kno) values(now(),'16301023','丁正卿','4');
