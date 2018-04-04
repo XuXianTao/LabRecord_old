02ca8b1a77aa231470c51ac595a5998a11d254fe
ModyQyw <261857860@qq.com>
2018-04-04 19:13:12 +0800

    v2.20 大更新
    1、增添页面与对应css，请查看对应readme.txt
    2、更新4.3需求文档
    3、小部分补充php

--- lab_sys/Home/Controller/FeedbackController.class.php
+++ lab_sys/Home/Controller/FeedbackController.class.php
@@ -13,20 +13,34 @@ class FeedbackController extends Controller {
     }
 	public function fb_(){
     	$knowc=I('knowledge');
-    	if ($knowc){
+    	if ($knowc) {
     		$Fb=D('fb');
             $Fb->create();
-            if ($Fb->where('id='.session('user')['id'])->select())$Fb->save();
+            if ($Fb->where('id='.session('user')['id'])->select()) $Fb->save();
             else $Fb->add();
             //$this->show("");
-            $this->redirect('main/main','',0.01,'<script>alert(\'感谢反馈\');</script>');
-    	}else{$this->error('不能为空');}
+            $this->redirect('main/main', '', 0.01, '<script>alert(\'感谢反馈\');</script>');
+    	} else {
+            $this->error('不能为空');
+        }
+    }
+    public function fbcre() {
+        $this->display();
+    }
+    public function fbrls() {
+        $this->display();
+    }
+    public function fbupdt() {
+        $this->display();
+    }
+    public function fbman() {
+        $this->display();
     }
     public function fbsts(){
         $admin=session('admin');
         if ($admin){
             $this->assign('admin',$admin);
             $this->display();
-        }else $this->redirect('logm');
+        } else $this->redirect('logm');
     }
 }
