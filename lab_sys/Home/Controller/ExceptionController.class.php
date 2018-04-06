757b46cf3e0f35e0a1e789bda66519f0da1f2ac2
fall0death <503703217@qq.com>
2018-04-06 15:58:28 +0800

    把bd取消，加进excp里面，把故障的部分合到一起

--- lab_sys/Home/Controller/ExceptionController.class.php
+++ lab_sys/Home/Controller/ExceptionController.class.php
@@ -65,4 +65,23 @@ class ExceptionController extends Controller {
             $this->redirect('logm');
         }
     }
+    public function excpsta($btn = 1){
+        if($btn<1){
+            $btn=1;
+        }
+        $page = ($btn-1) * 10;
+        $bd=M('excpsta');
+        $num_list = $bd->count();
+        $num = ceil($num_list / 10);
+        if($page>=$num_list)$page-=10;
+        $this->assign('page',$page/10+1);
+        $list=$bd->limit($page,10)->select();
+        $this->assign('list',$list);
+        $this->assign('num',$num);
+        $admin=session('admin');
+        if ($admin){
+            $this->assign('admin',$admin);
+    	    $this->display();
+        }else $this->redirect('logm');
+    }
 }
